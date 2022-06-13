4407f4a23a70d5b9bbf67a274b1301a1659167c0
Sol Turipe <solcis.bio@gmail.com>
2022-06-13 22:45:20 +0200

    link img right

--- game.js
+++ game.js
@@ -5,7 +5,9 @@ kaboom({
   debug: true,
 });
 
+loadRoot();
 loadSprite("link-going-left", "https://i.imgur.com/eiY5zyX.png");
+loadSprite("link-going-left", "https://i.imgur.com/yZIb8O2.png");
 
 scene("game", () => {});
 
