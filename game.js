528457862cfed6869b54fbd5a5ec0c11944355ce
Sol Turipe <solcis.bio@gmail.com>
2022-06-13 23:20:49 +0200

    more more images

--- game.js
+++ game.js
@@ -22,9 +22,9 @@ loadSprite("top-door", "U9nre4n.png");
 loadSprite("fire-pot", "I7xSp7w.png");
 loadSprite("left-door", "okdJNls.png");
 loadSprite("lanterns", "wiSiY09.png");
-loadSprite("slicer", "");
-loadSprite("skeletor", "");
-loadSprite("kaboom", "");
+loadSprite("slicer", "c6JFi5Z.png");
+loadSprite("skeletor", "Ei1VnX8.png");
+loadSprite("kaboom", "o9WizfI.png");
 loadSprite("stairs", "");
 loadSprite("bg", "");
 
